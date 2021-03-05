cbbedc51da717e5c5be3c07ede526a252fc686a5
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-03-05 14:11:25 -0500

    Finished lab.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,22 +4,38 @@ class LoginForm extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      username: undefined,
+      password: undefined
+    };
+  }
+
+  handleFormChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    if(!!this.state.username && !!this.state.password){
+      this.props.handleLogin(this.state)
+    }
   }
 
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" />
+            <input id="username" name="username" onChange={this.handleFormChange} type="text" />
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" />
+            <input id="password" name="password" onChange={this.handleFormChange} type="password" />
           </label>
         </div>
         <div>
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,14 +4,25 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      charCount: 0,
+      tweet: undefined
+    };
+  }
+
+  handleInputchange = (e) => {
+    this.setState({
+      charCount: e.target.value.length,
+      tweet: e.target.value
+    })
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input type="text" name="message" value={this.state.tweet} onChange={this.handleInputchange} id="message" /><br/>
+        <span>{this.props.maxChars - this.state.charCount} characters remaining</span><br/>
       </div>
     );
   }
